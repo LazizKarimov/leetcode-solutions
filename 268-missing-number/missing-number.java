36e67d7481c7345244445faed6a1612c44dc715d
Laziz Karimov <43858758+LazizKarimov@users.noreply.github.com>
2026-06-16 14:31:27 +0300

    Time: 0 ms (100.00%) | Memory: 47.5 MB (31.48%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+        int i = 0;
+        int maxNum = nums[i];
+        int sumNums = 0;
+        int len = nums.length;
+
+        while (i < len) {
+            sumNums += nums[i];
+            if (nums[i] > maxNum) {
+                maxNum = nums[i];
+            }
+            i++;
+        }
+
+        int sumNums2 = 0;
+        for(int j = 0; j <= nums.length; j++) {
+            sumNums2 += j;
+        }
+
+        return sumNums2 - sumNums;
+        
+    }
+}
