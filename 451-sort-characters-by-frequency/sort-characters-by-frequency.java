8913dfb91ff3a4182a39d749e3101037ccdec49c
Laziz Karimov <43858758+LazizKarimov@users.noreply.github.com>
2026-07-03 17:05:07 +0300

    Time: 29 ms (9.68%) | Memory: 53.5 MB (5.25%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,32 +1,18 @@
 class Solution {
     public String frequencySort(String s) {
-        char[] chars = s.toCharArray();
-        Map<Character, Integer> map = new HashMap<>();
-        
-        for (char c : chars) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-        
-        StringBuilder result = new StringBuilder();
-        
-        while (!map.isEmpty()) {
-            char maxChar = ' ';
-            int maxCount = 0;
-            
-            for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-                if (entry.getValue() > maxCount) {
-                    maxCount = entry.getValue();
-                    maxChar = entry.getKey();
-                }
-            }
-            
-            for (int i = 0; i < maxCount; i++) {
-                result.append(maxChar);
-            }
-            
-            map.remove(maxChar);
-        }
-        
-        return result.toString();
+        return new StringBuilder(
+            s.chars()
+             .boxed()
+             .collect(Collectors.groupingBy(
+                 Function.identity(), 
+                 Collectors.counting()
+             ))
+             .entrySet()
+             .stream()
+             .sorted((a, b) -> b.getValue().compareTo(a.getValue()))
+             .map(e -> String.valueOf((char) e.getKey().intValue())
+                 .repeat(e.getValue().intValue()))
+             .collect(Collectors.joining())
+        ).toString();
     }
 }
